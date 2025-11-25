2c87eca908e57b39aec42299340ecead71757b37
yvonnenyambura <yvonnenyambura>
2025-11-25 04:10:58 +0300

    add changes

--- socialmediaproject/settings.py
+++ socialmediaproject/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
 
 # Default primary key field type
